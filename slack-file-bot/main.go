c2a3108285bd708316da070256fef7303810565f
J0SAL <joyalmeida321@gmail.com>
2023-05-07 17:37:27 +0530

    added slack bot for upload file from local

---
+++ slack-file-bot/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-token")
+	os.Setenv("CHANNEL_ID", "channel_id")
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"joy_almeida_resume.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+
+		file, err := api.UploadFile(params)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Name %s, URL %s\n", file.Name, file.URL)
+	}
+
+}
